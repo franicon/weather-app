314a1ee2da27a56ad76197bf60b08c3b2dab38f5
Abayomi Aluko <franicons@gmail.com>
2023-07-16 15:26:55 +0100

    prep for deploy1

--- public/js/app.js
+++ public/js/app.js
@@ -10,7 +10,7 @@ weatherForm.addEventListener('submit', (e) => {
 
     msgOne.textContent = 'loading...'
 
-    return fetch('http://localhost:3000/weather?address='+location).then((res) => {
+    return fetch('/weather?address='+location).then((res) => {
         res.json().then((data) => {
             if (data.error) {
               msgOne.textContent = data.error
--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 // Define paths for Express Config
 const publicDir = path.join(__dirname, '../public')
@@ -82,6 +83,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('server running on port 3000')
 })
